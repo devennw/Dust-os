64cf1f1b7841c72a241678d5d1db4a0274a17abf
Devennw <dvennote65@gmail.com>
2026-07-18 09:38:31 +0700

    Comming Soon!

---
+++ DNU/dustkrnl/Include/dtos.h
@@ -0,0 +1,49 @@
+#ifndef _DTOS_H
+#define _DTOS_H
+
+#include "dustkrnl.h"
+#include "dustrtl.h"
+#include "dtosdef.h"
+#include 
+
+#if defined(__x86_64__)
+#include "x64.h"
+
+#elif defined(__aarch64__)
+#include "arm64.h"
+
+#elif defined(__i386__)
+#include "i386.h"
+
+#else
+#error "Architecture is not supported by Dust os"
+#endif // amd64
+
+#include "intrlk.h"
+#include "arc.h"
+#include "ke.h"
+#include "kd.h"
+#include "ex.h"
+#include "ps.h"
+#include "se.h"
+#include "io.h"
+#include "ob.h"
+#include "mm.h"
+#include "lpc.h"
+#include "dbgk.h"
+#include "lfs.h"
+#include "cache.h"
+#include "pnp.h"
+#include "hal.h"
+#include "kx.h"
+#include "cm.h"
+#include "po.h"
+#include "perf.h"
+#include "schad.h"
+#include "syscall.h"
+#include "wmi.h"
+#include "verifier.h"
+
+#define _DUSTDDK_
+
+#ifdef _DUSTDRIVER_
