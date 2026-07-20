d40fe675def2cd9ebfa33c2cf4f4f1ca80441e7c
Devennw <dvennote65@gmail.com>
2026-07-20 18:35:51 +0700

    [dustkrnl] modification system debug for kernel system

--- DNU/dustkrnl/Include/dbgk.h
+++ DNU/dustkrnl/Include/dbgk.h
@@ -1,15 +1,15 @@
-#ifndef _DBGK_DEBUG_
-#define _DBGK_DEBUG_
+#ifndef _DBGK_
+#define _DBGK_
 
-#define DBGK_THREAD_DEBUG     0x1
-#define DBGK_OBJECT_DEBUG     0x2
-#define DBGK_PROCESS_DEBUG    0x3
-#define DBGK_MESSAGE_DEBUG    0x4
-#define DBGK_EXCEPTION_DEBUG  0x5
+#define DBGK_THREAD     0x1
+#define DBGK_OBJECT     0x2
+#define DBGK_PROCESS    0x3
+#define DBGK_MESSAGE    0x4
+#define DBGK_EXCEPTION  0x5
 
 /* debug tracing support */
 
-#if _DBGK_DEBUG_
+#if _DBGK_
 #ifndef NEW_DEBUG_SYSTEM
 #define DBGK_TRACE(x, ...) {
       DbgkPrint("%s [%.16s] - ",
