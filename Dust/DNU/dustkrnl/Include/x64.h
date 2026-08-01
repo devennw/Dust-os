e8aba8b2278b83a5d940afbcce3e79ca8a44f417
Devennw <dvennote65@gmail.com>
2026-08-01 13:49:43 +0700

    [Include] [amd64] Placing a code so that the kernel can run smoothly on the amd64 architecture

--- Dust/DNU/dustkrnl/Include/x64.h
+++ Dust/DNU/dustkrnl/Include/x64.h
@@ -12,16 +12,16 @@ KeCompactServiceTable (
 
 /* header mechine architecture */
 
-#define IMAGE_HEADER_FILE_MECHINE_NATIVE   // harus diisi dengan hexa desimal
-
+#define IMAGE_HEADER_FILE_MECHINE_NATIVE  0x8cb5
 #endif
 
 /* end dustsp */
 
-#if // harus diisi setelah menambahkan function 
+#if (!(defined(_DUST_DRIVER_)) || defined(_DUSTDDK_) || defined(_DUSTIFS_) \
+    || defined(_DUSTHAL_) || defined(_DUSTSP_)) && !defined(_BLDR_)
 
-#define 
-#define 
+#define exRaiseException    rtlRaiseException
+#define exRaiseStatus       rtlRaiseStatus
 
 #endif 
 
@@ -31,5 +31,38 @@ KeCompactServiceTable (
 extern "C" {
 #endif 
 
+UCHAR
+__inbyte (
+    IN USHORT port
+    );
 
+USHORT
+__inword (
+    IN USHORT port
+    );
+
+ULONG
+__inlong (
+    IN USHORT port
+    );
+
+VOID
+__outbyte (
+    IN USHORT port
+    );
+
+VOID
+__outword (
+    IN USHORT port
+)
+
+VOID
+__outlong (
+    IN USHORT port
+)
+
+
+
+#ifndef __cplusplus
 }
+#endif
